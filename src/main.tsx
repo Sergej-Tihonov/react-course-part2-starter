3002c5c96846c9ec30fa5b758f5d122f9e6ff007
Sergej Tihonov <sergej@visifo.com>
2023-11-28 15:06:28 +0100

    custom default query options

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,18 @@ import App from './App';
 import './index.css';
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3,
+      cacheTime: 300_000, // 5 minutes
+      staleTime: 10 * 1000, // 10 seconds
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+    }
+  }
+});
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
