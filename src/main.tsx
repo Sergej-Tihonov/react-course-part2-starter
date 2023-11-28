d172900aa8005439c5f983b0fb49b268d14e83c5
Sergej Tihonov <sergej@visifo.com>
2023-11-28 15:07:37 +0100

    set stale time per query

--- src/main.tsx
+++ src/main.tsx
@@ -6,18 +6,7 @@ import App from './App';
 import './index.css';
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: 3,
-      cacheTime: 300_000, // 5 minutes
-      staleTime: 10 * 1000, // 10 seconds
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-      refetchOnMount: false,
-    }
-  }
-});
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
