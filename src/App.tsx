0838c733e0baba197eabe9f2a27c5ba8914b8bbb
Sergej Tihonov <sergej@visifo.com>
2023-12-04 20:53:50 +0100

    adding new todo entry via mutation from react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css';
 import TodoList from "./react-query/TodoList";
 import PostList from "./react-query/PostList";
+import TodoForm from "./react-query/TodoForm";
 
 function App() {
   return (
     <>
       <h1>React Starter Project</h1>
-      {/*<TodoList/>*/}
-      <PostList/>
+      <TodoForm/>
+      <TodoList/>
+      {/*<PostList/>*/}
     </>
   );
 }
--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,10 +1,32 @@
 import { useRef } from 'react';
+import {useMutation, useQueryClient} from "@tanstack/react-query";
+import {Todo} from "../hooks/useTodos";
+import axios from "axios";
 
 const TodoForm = () => {
+  const queryClient = useQueryClient();
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
+        .then(res => res.data),
+    onSuccess: (savedTodo, newTodo) => {
+      queryClient.setQueryData<Todo[]>(['todos'], (oldTodos) => [savedTodo, ...(oldTodos || [])])
+    }
+  })
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="row mb-3">
+    <form className="row mb-3" onSubmit={event => {
+      event.preventDefault();
+      if (!ref.current?.value) return;
+      addTodo.mutate({
+        id: 0,
+        title: ref.current?.value || '',
+        userId: 1,
+        completed: false,
+      })
+    }}>
       <div className="col">
         <input ref={ref} type="text" className="form-control" />
       </div>
